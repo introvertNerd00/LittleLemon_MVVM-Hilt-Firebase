8b82acbcf0f3d5fda0126e721278df5965bbc576
Hamza <hamza.afzalkhan9@gmail.com>
2023-06-04 02:46:07 +0500

    change in main color

--- app/src/main/java/com/example/littlelemon/ui/theme/Color.kt
+++ app/src/main/java/com/example/littlelemon/ui/theme/Color.kt
@@ -4,7 +4,7 @@ import androidx.compose.ui.graphics.Color
 
 object LittleLemonColor {
     val green = Color(0xFF61A28C)
-    val yellow = Color(0xFFBB9E0F)
+    val yellow = Color(0xFFE7C622)
     val pink = Color(0xFFD3B79C)
     val cloud = Color(0xFFEDEFEE)
     val charcoal = Color(0xFF333333)
